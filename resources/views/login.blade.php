1bc490e560b6d76e5d822e6547abcb1328416031
Muhammad Anas <anas170645@gmail.com>
2023-06-08 09:10:28 +0500

    particles

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,9 +15,9 @@
 
 <body>
     <div class="main-wrapper">
-        <div class="login-page">
+        <div class="login-page" id="tsparticles">
             <div class="login-body container">
-                <div class="loginbox">
+                <div class="loginbox" style="border: 5px solid #a67a3d;">
                     <div class="login-right-wrap">
                         <div class="account-header">
                             <div class="account-logo text-center mb-4">
@@ -61,8 +61,113 @@
         </div>
     </div>
     <script src="{{ asset('admin/assets/js/jquery-3.6.0.min.js') }}"></script>
+    <!-- first include tsParticles engine -->
+<script src="https://cdn.jsdelivr.net/npm/tsparticles-engine"></script>
+
+<!-- then include any tsParticles plugin needed -->
+<script src="https://cdn.jsdelivr.net/npm/tsparticles/tsparticles.bundle.min.js"></script>
+
+<!-- then include jquery wrapper -->
+<script src="https://cdn.jsdelivr.net/npm/jquery-particles"></script>
     <script src="{{ asset('admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('admin/assets/js/admin.js') }}"></script>
+    <script>
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+// starting from v2 you can add only the features you need reducing the bundle size
+$(document).ready(async function () {
+    await loadFull(tsParticles);
+
+    $("#tsparticles")
+        .particles()
+        .init(
+            {
+                background: {
+                    color: {
+                        value: "#0d47a1",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            },
+            function (container) {
+                // container is the particles container where you can play/pause or stop/start.
+                // the container is already started, you don't need to start it manually.
+            }
+        );
+
+    // or
+
+    $("#tsparticles")
+        .particles()
+        .ajax("assets/particles.json", function (container) {
+            // container is the particles container where you can play/pause or stop/start.
+            // the container is already started, you don't need to start it manually.
+        });
+});
+    </script>
 </body>
 
 </html>
